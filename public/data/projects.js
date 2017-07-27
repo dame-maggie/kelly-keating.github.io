e6a376a6230a3eb80bcf2e1faf3759a5e5b0008b
kelly-keating <keating.kel@gmail.com>
2017-07-27 17:21:40 +1200

    Some examples added

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,3 +1,5 @@
 modeule.exports = [
-  {name: 'gitmaster'}
+  {name: 'The Gitmaster', blurb:'We made a game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'EDA cheatsheet', blurb:'Toggle!', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'CLOCK', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'}
 ]
