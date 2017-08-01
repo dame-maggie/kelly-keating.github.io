44616b02a80a30cf16586d5a3f3b6bae7e532401
kelly-keating <keating.kel@gmail.com>
2017-08-01 17:10:01 +1200

    title test fixed

--- test/index.test.js
+++ test/index.test.js
@@ -7,5 +7,5 @@ import Title from '../client/components/Title'
 
 test('Heading renders on App', t => {
   const wrapper = shallow(<Title />)
-  t.is(wrapper.find('h1').text(), 'Kelly Keating')
+  t.is(wrapper.find('h1').first().text(), 'Kelly Keating')
 })
