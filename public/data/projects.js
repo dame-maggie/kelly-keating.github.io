d50f1adbbaeb508d1e48a3949d125e5cb72f3a78
kelly-keating <keating.kel@gmail.com>
2017-08-29 16:13:00 +1200

    Project list increased

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,5 +1,10 @@
 module.exports = [
   {name: 'The Gitmaster', blurb:'We made a game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
   {name: 'EDA cheatsheet', blurb:'Toggle!', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'CLOCK', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'}
+  {name: 'CLOCK', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Jukebox', blurb:'Can search and create playlists.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'KanyeRest', blurb:'Simple use of a RESTful API.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Calculator', blurb:'Numbery buttons.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Minesweeper', blurb:'Simple game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Tic Tac Toe', blurb:'Classic.', image:'http://i.imgur.com/2WYjdQt.jpg'}
 ]
