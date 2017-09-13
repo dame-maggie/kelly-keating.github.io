0c22919a6bcc9556288203df057359d4f293c645
kelly-keating <keating.kel@gmail.com>
2017-09-13 16:45:26 +1200

    Title rearranged

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Title = () => (
   <div className='title-container'>
-    <img src='http://i.imgur.com/NjcGgJi.jpg' className='title-img'/>
+    <img src='http://i.imgur.com/2WYjdQt.jpg' id='title-logo'/>
     <h1>Kelly Keating</h1>
-    <h1>(Currently under construction)</h1>
+    <p>I am a recent graduate of Enspiral Dev Academy, Javascript coder, maker of websites, and creator of games such as The Gitmaster and PurrPolice.</p>
+    <p>My love of code and creating interactions came from an enthusiasm for puzzles. I love trying to work my way around a problem, finding simple solutions in complex systems, and being able to produce something awesome at the end of it.</p>
+  <img src='http://i.imgur.com/NjcGgJi.jpg' id='title-img'/>
     // My skills
   </div>
 )
--- public/bundle.js
+++ public/bundle.js
@@ -12582,17 +12582,23 @@ var Title = function Title() {
   return _react2.default.createElement(
     'div',
     { className: 'title-container' },
-    _react2.default.createElement('img', { src: 'http://i.imgur.com/NjcGgJi.jpg', className: 'title-img' }),
+    _react2.default.createElement('img', { src: 'http://i.imgur.com/2WYjdQt.jpg', id: 'title-logo' }),
     _react2.default.createElement(
       'h1',
       null,
       'Kelly Keating'
     ),
     _react2.default.createElement(
-      'h1',
+      'p',
       null,
-      '(Currently under construction)'
+      'I am a recent graduate of Enspiral Dev Academy, Javascript coder, maker of websites, and creator of games such as The Gitmaster and PurrPolice.'
     ),
+    _react2.default.createElement(
+      'p',
+      null,
+      'My love of code and creating interactions came from an enthusiasm for puzzles. I love trying to work my way around a problem, finding simple solutions in complex systems, and being able to produce something awesome at the end of it.'
+    ),
+    _react2.default.createElement('img', { src: 'http://i.imgur.com/NjcGgJi.jpg', id: 'title-img' }),
     '// My skills'
   );
 };
