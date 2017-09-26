6ed00abc87a11ed1ecadfd996db1feb5358decf7
kelly-keating <keating.kel@gmail.com>
2017-09-26 16:06:58 +1300

    good fonts found

--- public/data/projects.js
+++ public/data/projects.js
@@ -1,10 +1,10 @@
 module.exports = [
-  {name: 'The Gitmaster', blurb:'We made a game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'EDA cheatsheet', blurb:'Toggle!', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'CLOCK', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Jukebox', blurb:'Can search and create playlists.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'KanyeRest', blurb:'Simple use of a RESTful API.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Calculator', blurb:'Numbery buttons.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Minesweeper', blurb:'Simple game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Tic Tac Toe', blurb:'Classic.', image:'http://i.imgur.com/2WYjdQt.jpg'}
+  {name: 'The Gitmaster', alias: 'gitmaster', blurb:'We made a game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'EDA cheatsheet', alias: 'cheatsheet', blurb:'Toggle!', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'CLOCK', alias: 'clock', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Jukebox', alias: 'jukebox', blurb:'Can search and create playlists.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'KanyeRest', alias: 'kanye', blurb:'Simple use of a RESTful API.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Calculator', alias: 'calculator', blurb:'Numbery buttons.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Minesweeper', alias: 'minesweeper', blurb:'Simple version of the game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Tic Tac Toe', alias: 'tictactoe', blurb:'Classic.', image:'http://i.imgur.com/2WYjdQt.jpg'}
 ]
