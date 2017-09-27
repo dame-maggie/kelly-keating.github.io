8e05b3b2123e49050e1e75872ca4bcef26acfa43
kelly-keating <keating.kel@gmail.com>
2017-09-27 18:01:01 +1300

    Secure image link

--- public/data/projects.js
+++ public/data/projects.js
@@ -2,9 +2,9 @@ module.exports = [
   {name: 'The Gitmaster', alias: 'gitmaster', blurb:'We made a game.', href:'https://thegitmaster.herokuapp.com/', image:'https://imgur.com/MuCW4BM.jpg'},
   {name: 'EDA cheatsheet', alias: 'cheatsheet', blurb:'Toggle!', href:'https://eda-cheatsheet.herokuapp.com/', image:'https://imgur.com/DIi6DOB.jpg'},
   {name: 'CLOCK', alias: 'clock', blurb:'Tells the time.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Jukebox', alias: 'jukebox', blurb:'Can search and create playlists.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'KanyeRest', alias: 'kanye', blurb:'Simple use of a RESTful API.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Calculator', alias: 'calculator', blurb:'Numbery buttons.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Minesweeper', alias: 'minesweeper', blurb:'Simple version of the game.', image:'http://i.imgur.com/2WYjdQt.jpg'},
-  {name: 'Tic Tac Toe', alias: 'tictactoe', blurb:'Classic.', image:'http://i.imgur.com/2WYjdQt.jpg'}
+  {name: 'Jukebox', alias: 'jukebox', blurb:'Can search and create playlists.', image:'https://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'KanyeRest', alias: 'kanye', blurb:'Simple use of a RESTful API.', image:'https://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Calculator', alias: 'calculator', blurb:'Numbery buttons.', image:'https://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Minesweeper', alias: 'minesweeper', blurb:'Simple version of the game.', image:'https://i.imgur.com/2WYjdQt.jpg'},
+  {name: 'Tic Tac Toe', alias: 'tictactoe', blurb:'Classic.', image:'https://i.imgur.com/2WYjdQt.jpg'}
 ]
